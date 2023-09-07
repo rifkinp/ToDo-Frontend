6f1b24836091fa5fcfd6ad63664b79507fa6cee0
Rifki Nugraha <rifkichocobo@gmail.com>
2023-09-07 14:43:28 +0800

    refactor : delete todo link in homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,6 @@ export default function Home() {
             Login
           </button>
         </Link>
-        <Link href='/todo'>
-          <button className='px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600'>
-            Todo List
-          </button>
-        </Link>
         <Link href='/registration'>
           <button className='px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600'>
             Register
